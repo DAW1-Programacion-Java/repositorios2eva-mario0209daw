9a2698f3a14a9f30cf056d21531c370e5dd5aff6
mario0209 <mguerreros09@educarex.es>
2025-01-15 18:45:17 +0100

    primer commit

--- src/Tema4/ejercicio1/Alumno.java
+++ src/Tema4/ejercicio1/Alumno.java
@@ -1,1 +1,12 @@
+package Tema4.ejercicio1;
 
+public class Alumno {
+    private String Nombre;
+    private String Apellido1;
+    private String Apellido2;
+    private String Numero_expediente;
+    private String Fecha_nacimiento;
+    private String curso;
+    private double nota1;
+    private double nota2;
+}
